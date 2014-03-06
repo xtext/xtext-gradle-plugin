96259be00ff88449fdf63db8c4eb98f99b5aa9a8
Stefan Oehme <st.oehme@gmail.com>
2014-03-06 16:03:55 +0100

    Enhanced DSL with source folders, automatically download correct xtext version

--- src/main/groovy/org/xtext/gradle/XtextPlugin.groovy
+++ src/main/groovy/org/xtext/gradle/XtextPlugin.groovy
@@ -1,25 +1,32 @@
 package org.xtext.gradle;
 
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.capitalize
+import javax.inject.Inject;
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.BasePlugin;
-import org.xtext.gradle.tasks.Language
+import org.gradle.api.internal.file.FileResolver
+import org.gradle.api.plugins.BasePlugin
 import org.xtext.gradle.tasks.XtextExtension
 import org.xtext.gradle.tasks.XtextGenerate
 
 class XtextPlugin implements Plugin<Project> {
 
+	FileResolver fileResolver
+
+	@Inject
+	XtextPlugin(FileResolver fileResolver) {
+		this.fileResolver = fileResolver
+	}
+
 	def void apply(Project project) {
 		project.plugins.apply(BasePlugin)
+		def XtextExtension xtext = project.extensions.create("xtext", XtextExtension, project, fileResolver);
 		project.configurations.create("xtext")
-		def XtextExtension xtext = project.extensions.create("xtext", XtextExtension, project);
-		xtext.languages.all{Language language ->
-			def task = project.tasks.create("generate${language.name.capitalize()}") { 
-				println("This is the Xtext plugin, generating for the ${language.name.capitalize()} language") 
-			}
-			project.tasks[BasePlugin.ASSEMBLE_TASK_NAME].dependsOn(task)
+		project.afterEvaluate{
+			def XtextGenerate generatorTask = project.tasks.create("xtextGenerate", XtextGenerate)
+			generatorTask.configure(xtext)
+			project.tasks[BasePlugin.ASSEMBLE_TASK_NAME].dependsOn(generatorTask)
+			project.dependencies.add("xtext", "org.eclipse.xtext:org.eclipse.xtext:${xtext.version}")
 		}
 	}
 }
--- src/main/groovy/org/xtext/gradle/tasks/XtextExtension.groovy
+++ src/main/groovy/org/xtext/gradle/tasks/XtextExtension.groovy
@@ -2,57 +2,68 @@ package org.xtext.gradle.tasks;
 
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
+import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.internal.FactoryNamedDomainObjectContainer
+import org.gradle.api.internal.file.DefaultSourceDirectorySet
+import org.gradle.api.internal.file.FileResolver
 import org.gradle.internal.reflect.Instantiator
+import org.gradle.util.ConfigureUtil;
 
 class XtextExtension {
+	String version = "2.5.3"
+	String encoding = "UTF-8"
+	SourceDirectorySet sources
 	NamedDomainObjectContainer<Language> languages;
-	String encoding
-	String xtextVersion
+	
+	private Project project
 
-	XtextExtension(Project project) {
+	XtextExtension(Project project, FileResolver fileResolver) {
+		this.project = project
 		languages = project.container(Language){name -> new Language(project, name)}
+		sources = new DefaultSourceDirectorySet("xtext", fileResolver)
 	}
 
 	def languages(Closure closure) {
 		languages.configure(closure)
 	}
+	
+	def sources(Closure closure) {
+		ConfigureUtil.configure(closure, sources)
+	}
+	
 }
 
 class Language {
 	String name;
 	String setup
-	boolean useJavaSupport
-	NamedDomainObjectContainer<OutputConfiguration> outputConfigurations
+	boolean consumesJava
+	NamedDomainObjectContainer<OutputConfiguration> outputs
 
 	private Project project
 
 	Language(Project project, String name) {
 		this.name = name
 		this.project = project
-		outputConfigurations = project.container(OutputConfiguration)
+		outputs = project.container(OutputConfiguration)
 	}
 
-	def outputConfigurations(Closure closure) {
-		outputConfigurations.configure(closure)
+	def outputs(Closure closure) {
+		outputs.configure(closure)
 	}
 	
-	def OutputConfiguration getOutputConfiguration() {
-		outputConfigurations.maybeCreate("DEFAULT_CONFIGURATION")
+	def OutputConfiguration getOutput() {
+		outputs.maybeCreate("DEFAULT_CONFIGURATION")
 	}
 	
-	def outputConfiguration(Closure closure) {
-		outputConfigurations.configure(closure)
-	}
-	
-	def setOutputDirectory(Object outputDirectory) {
-		getOutputConfiguration().setOutputDirectory(outputDirectory)
+	def output(Closure closure) {
+		ConfigureUtil.configure(closure, getOutput())
 	}
 }
 
 class OutputConfiguration {
 	String name
-	def outputDirectory
+	def dir
+	boolean producesJava
 
 	OutputConfiguration(String name) {
 		this.name = name
