b370ce85c7141697c44759bd2f8af26e145edacc
Stefan Oehme <st.oehme@gmail.com>
2014-03-06 16:19:43 +0100

    Add main java sources to Xtext sources if a language needs Java and the Java plugin is found

--- src/main/groovy/org/xtext/gradle/XtextPlugin.groovy
+++ src/main/groovy/org/xtext/gradle/XtextPlugin.groovy
@@ -6,6 +6,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.file.FileResolver
 import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.SourceSet;
 import org.xtext.gradle.tasks.XtextExtension
 import org.xtext.gradle.tasks.XtextGenerate
 
@@ -24,6 +27,11 @@ class XtextPlugin implements Plugin<Project> {
 		project.configurations.create("xtext")
 		project.afterEvaluate{
 			def XtextGenerate generatorTask = project.tasks.create("xtextGenerate", XtextGenerate)
+			def java = project.convention.findPlugin(JavaPluginConvention)
+			def needJava = xtext.languages.any {it.consumesJava}
+			if (needJava && java != null) {
+				xtext.sources.source(java.sourceSets.main.java)
+			}
 			generatorTask.configure(xtext)
 			project.tasks[BasePlugin.ASSEMBLE_TASK_NAME].dependsOn(generatorTask)
 			project.dependencies.add("xtext", "org.eclipse.xtext:org.eclipse.xtext:${xtext.version}")
